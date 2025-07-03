2fc92d4fd696b39256cfa3ed67cbc39acb4073de
Manujayaa <janithrathnayake01@gmail.com>
2025-07-03 20:19:17 +0530

    spring jdbc structure

---
+++ src/main/java/com/manujaya/SpringJDBCEx/SpringJdbcExApplication.java
@@ -0,0 +1,23 @@
+package com.manujaya.SpringJDBCEx;
+
+import com.manujaya.SpringJDBCEx.model.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringJdbcExApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context =SpringApplication.run(SpringJdbcExApplication.class, args);
+		//set Student object
+		Student st = context.getBean(Student.class);
+		//set values
+		st.setRollNo(1);
+		st.setName("manujaya");
+		st.setMarks(80);
+	}
+
+
+}

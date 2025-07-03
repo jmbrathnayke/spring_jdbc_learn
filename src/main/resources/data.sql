8251fff9925831fadf558fc144ea9d89a212672d
Manujayaa <janithrathnayake01@gmail.com>
2025-07-04 01:37:33 +0530

    fix and modify schema.sql file and data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO Student (rollNo, name , marks) VALUES (101,"manujaya",80);
-INSERT INTO Student (rollNo, name , marks) VALUES (102,"janith",76);
-INSERT INTO Student (rollNo, name , marks) VALUES (103,"yash",70);
+INSERT INTO Student (rollNo, name , marks) VALUES (101,'manujaya',80);
+INSERT INTO Student (rollNo, name , marks) VALUES (102,'janith',76);
+INSERT INTO Student (rollNo, name , marks) VALUES (103,'yash',70);

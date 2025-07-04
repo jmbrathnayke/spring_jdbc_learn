7ed151d53e2c275687d3771fabbd1bc4db9f6dda
Manujayaa <janithrathnayake01@gmail.com>
2025-07-04 10:19:21 +0530

    fetch data with lambda expression and without lambda

--- src/main/java/com/manujaya/SpringJDBCEx/repo/StudentRepo.java
+++ src/main/java/com/manujaya/SpringJDBCEx/repo/StudentRepo.java
@@ -1,16 +1,24 @@
 package com.manujaya.SpringJDBCEx.repo;
-import com.manujaya.SpringJDBCEx.model.Student;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.manujaya.SpringJDBCEx.model.Student;
+
 @Repository
 public class StudentRepo {
+
     private JdbcTemplate jdbc;
 
-    //getters and setters
+
+
     public JdbcTemplate getJdbc() {
         return jdbc;
     }
@@ -20,18 +28,51 @@ public class StudentRepo {
         this.jdbc = jdbc;
     }
 
+    public void save(Student s) {
+        String sql="insert into student(rollno,name,marks) values(?,?,?)";
 
-    //save method
-    public void save(Student st) {
-
-        String sql = "insert into Student (rollNo, name ,marks) Values (?,?,?)";
-        int rows = jdbc.update(sql, st.getRollNo(),st.getName(),st.getMarks());
-        System.out.println(rows + "effected");
+        int rows=jdbc.update(sql,s.getRollNo(),s.getName(),s.getMarks());
+        System.out.println(rows +"affected");
     }
 
 
-    public List<Student> findAll() {
-        List<Student> students = new ArrayList<>();
-        return students;
+
+//    //with lambda expression
+//    public List<Student> findAll() {
+//
+//        String sql="select * from student"; RowMapper<Student> mapper=(rs,rowNum) ->
+//        {
+//            Student s=new Student();
+//            s.setRollNo(rs.getInt("rollno"));
+//            s.setName(rs.getString("name"));
+//            s.setMarks(rs.getInt("marks"));
+//            return s;
+//
+//        };
+//
+//        return jdbc.query(sql, mapper);
+//
+//    }
+
+
+
+    //without lambda expression
+
+
+      public List<Student> findAll() {
+
+      String sql="select * from student"; RowMapper<Student> mapper=new RowMapper<Student>() {
+
+      @Override public Student mapRow(ResultSet rs, int rowNum) throws SQLException
+      {
+          Student s=new Student();
+          s.setRollNo(rs.getInt("rollno"));
+      s.setName(rs.getString("name"));
+      s.setMarks(rs.getInt("marks")); return s; }
+      };
+      return jdbc.query(sql, mapper);
     }
+
+
+
 }
